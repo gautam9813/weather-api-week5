0bcff800855d15d713a052bb144a96790e867384
gautam9813 <151638341+gautam9813@users.noreply.github.com>
2024-02-29 15:48:10 +0000

    src file creation

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { Console } from "console";
+import express from "express";
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.listen(PORT, () => {
+    console.log('server is listening on port ${PORT}');
+
+});
